04bfe36ad399845caae6bd63c63a78832bcacae2
oaj6634 <dhdjwlsdlaehk123@naver.com>
2024-03-29 10:24:53 +0900

    REFACTOR: add swagger

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,12 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional } from 'class-validator';
 
 export class ManifestQueryDto<T> {
+  @ApiProperty({
+    description: 'platform',
+    example: '.dmg',
+    type: 'string',
+  })
   @IsOptional()
   platform: T;
 
+  @ApiProperty({
+    description: 'version',
+    example: '0.1.9',
+  })
   @IsOptional()
   version?: string;
 
+  @ApiProperty({
+    description: 'releaseName',
+    example: 'foo-production',
+    required: false,
+  })
   @IsOptional()
   releaseName: string;
 }
